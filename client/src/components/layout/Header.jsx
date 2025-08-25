008dfdca4d556514be2278ea1803b7e6762ac970
Rohit <rohitmahto1122@gmail.com>
2025-08-25 14:07:29 +0530

    link to home

--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -29,7 +29,7 @@ const Header = () => {
   const wishlistItemCount = wishlist.length
 
   return (
-    <header className="bg-white shadow-md sticky top-0 z-50">
+    <header className="bg-[#F5EECC] shadow-md sticky top-0 z-50">
       {/* Top Header */}
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
